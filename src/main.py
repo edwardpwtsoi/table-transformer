782c6734bb8b7bc8dc956c129a419f9708f65b26
Edward Tsoi <edward.tsoi@apoidea.ai>
2023-04-03 16:14:10 +0800

    updated output dir

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ from datetime import datetime
 import string
 import sys
 import random
+from pathlib import Path
+
 import numpy as np
 import torch
 from torch import Generator
@@ -319,7 +321,7 @@ def train(args, model, criterion, postprocessors, device):
     # Create new save directory
     else:
         run_date = datetime.now().strftime("%Y%m%d%H%M%S")
-        output_directory = os.path.join(f"../{os.getcwd()}", "output", run_date)
+        output_directory = Path(os.getcwd()).parent.parent.joinpath("output", run_date)
 
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
